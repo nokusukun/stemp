85be22ccf1919d906304b17258cce2ab5da78198
nokusukun <nokusukun@yahoo.co.jp>
2019-07-19 14:12:09 +0800

    Added first commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# stemp
+Go simple string templates
+
+## Installation
+```bash
+go get github.com/nokusukun/stemp
+```
+
+## Usage
+```go
+package main
+
+import (
+	"github.com/nokusukun/stemp"
+)
+
+type Client struct {
+	Name string
+	BotName string
+}
+
+
+func main() {
+    myClient := stemp.Client{Name: "Noku", BotName: "RegBot"}
+    myMap := map[string]interface{}{"name": "Nokui", "botname": "Somalia"}
+
+    resultStruct := stemp.CompileStruct("Hello {Name}, I'm {BotName}. Nice to meet you {asdasds}. I'm accessing the values from a struct!", myClient)
+    
+    result := Compile("Hello {name}, I'm {botname}. I'm accessing the values from a map!", myMap)
+    
+    fmt.Println(resultStruct)
+    fmt.Println(result)
+}
+```
+
+### Result
+```
+Hello Noku, I'm RegBot. Nice to meet you {asdasds}. I'm accessing the values from a struct!
+Hello Nokui, I'm Somalia. I'm accessing the values from a map!
+```
+
+## Docs
+https://godoc.org/github.com/nokusukun/stemp
---
+++ stemp.go
@@ -0,0 +1,74 @@
+package stemp
+
+import (
+	"fmt"
+	"reflect"
+	"regexp"
+	"strings"
+)
+
+var findWords = regexp.MustCompile(`${\w+}`)
+
+// Compile - Compiles a string template given a map of [string]interface{}.
+func Compile(template string, maps map[string]interface{}) string {
+	words := findWords.FindAllString(template, -1)
+	for _, source := range words {
+		sourcetxt := source[1 : len(source)-1]
+		target, exists := maps[sourcetxt]
+		if exists {
+			template = strings.ReplaceAll(template, source, fmt.Sprintf("%v", target))
+		}
+	}
+	return template
+}
+
+// CompileStrict - Compiles a string template given a map of [string]interface{}.
+// Returns an error and the partial compiled string when it finds an invalid target.
+func CompileStrict(template string, maps map[string]interface{}) (string, error) {
+	words := findWords.FindAllString(template, -1)
+	hasError := false
+	for _, source := range words {
+		sourcetxt := source[1 : len(source)-1]
+		target, exists := maps[sourcetxt]
+		if exists {
+			template = strings.ReplaceAll(template, source, fmt.Sprintf("%v", target))
+		}
+	}
+	if hasError {
+		return template, fmt.Errorf("There are some invalid fields")
+	}
+	return template, nil
+}
+
+// CompileStruct - Compiles a string template given a struct.
+func CompileStruct(template string, maps interface{}) string {
+	words := findWords.FindAllString(template, -1)
+	for _, source := range words {
+		sourcetxt := source[1 : len(source)-1]
+		target := reflect.ValueOf(maps).FieldByName(sourcetxt)
+		if target.IsValid() {
+			template = strings.ReplaceAll(template, source, fmt.Sprintf("%v", target))
+		}
+	}
+	return template
+}
+
+// CompileStructStrict - Compiles a string template given a struct.
+// Returns an error and the partial compiled string when it finds an invalid target.
+func CompileStructStrict(template string, maps interface{}) (string, error) {
+	words := findWords.FindAllString(template, -1)
+	hasError := false
+	for _, source := range words {
+		sourcetxt := source[1 : len(source)-1]
+		target := reflect.ValueOf(maps).FieldByName(sourcetxt)
+		if target.IsValid() {
+			template = strings.ReplaceAll(template, source, fmt.Sprintf("%v", target))
+		} else {
+			hasError = true
+		}
+	}
+	if hasError {
+		return template, fmt.Errorf("There are some invalid fields")
+	}
+	return template, nil
+}
